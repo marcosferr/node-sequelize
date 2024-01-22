e3482b06e595df9a78b8fb76441a2f76eb8ee573
marcosferr <ferreiramarcosadrian@gmail.com>
2024-01-22 11:27:10 -0300

    re estructure project

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+
+const { PORT } = require("./util/config");
+const { connectToDatabase } = require("./util/db");
+
+const notesRouter = require("./controllers/notes");
+
+app.use(express.json());
+
+app.use("/api/notes", notesRouter);
+
+const start = async () => {
+  await connectToDatabase();
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+};
+
+start();
