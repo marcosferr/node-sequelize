f2682fdf68d0fb0eed187cce0845579455304743
marcosferr <ferreiramarcosadrian@gmail.com>
2024-01-23 11:01:25 -0300

    Add migration rollback util, and admin actions

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,20 +1,7 @@
 const router = require("express").Router();
-
+const tokenExtractor = require("../middleware/tokenExtractor");
 const { Note, User } = require("../models");
 const { Op } = require("sequelize");
-const tokenExtractor = (req, res, next) => {
-  const authorization = req.get("authorization");
-  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
-    try {
-      req.decodedToken = jwt.verify(authorization.substring(7), SECRET);
-    } catch {
-      return res.status(401).json({ error: "token invalid" });
-    }
-  } else {
-    return res.status(401).json({ error: "token missing" });
-  }
-  next();
-};
 
 const noteFinder = async (req, res, next) => {
   req.note = await Note.findByPk(req.params.id);
